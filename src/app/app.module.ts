066fea983744f83e6d57ba1fdea25abe14739729
404Nikhil <nicksd111@gmail.com>
2023-11-28 01:44:19 +0530

    added user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from  '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { UserComponent } from './user/user.component';
 import { RepositoryComponent } from './repository/repository.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    UserComponent,
     RepositoryComponent
   ],
   imports: [
